930d990c60f0b7fb7a403eb91fc62bbdf72f408f
Aleksandr Nikitin <NAI_surgut@mail.ru>
2024-11-09 10:48:27 +0500

    widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,4 @@
-#from masks import get_mask_account, get_mask_card_number
-from .masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(info_by_card: str) -> str:
