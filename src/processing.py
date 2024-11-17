8620967d600d54079c2ab28d893233fc5a3cb5ff
Aleksandr Nikitin <NAI_surgut@mail.ru>
2024-11-17 16:40:31 +0500

    homework 10_1 done

--- src/processing.py
+++ src/processing.py
@@ -19,7 +19,7 @@ def filter_by_state(list_dictionaries: list, state: str = "EXECUTED") -> list:
     return output_list
 
 
-# print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+#print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
 
 
 def sort_by_date(list_dictionaries: list, reverse: bool = True) -> list:
