10c2ccab005c044a157281770763e651041ca9a6
Aleksandr Nikitin <NAI_surgut@mail.ru>
2024-11-17 12:21:36 +0500

    funcs in processing

---
+++ src/processing.py
@@ -0,0 +1,41 @@
+def filter_by_state(list_dictionaries: list, state: str='EXECUTED') -> list:
+    """ 
+    Функция, которая принимает список словарей и опционально значение для ключа state
+    (по умолчанию 'EXECUTED'). Функция возвращает новый список словарей, содержащий только те словари, 
+    у которых ключ state соответствует указанному значению.
+
+    # Выход функции со статусом по умолчанию 'EXECUTED'
+    [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+    # Выход функции, если вторым аргументов передано 'CANCELED'
+    [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    """
+    output_list = []
+    for i in list_dictionaries:
+        if i['state'] == 'EXECUTED':
+            output_list.append(i)
+    return output_list
+
+
+# print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+
+
+def sort_by_date(list_dictionaries: list, reverse: bool=True) -> list:
+    """
+    Функция, которая принимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате
+    (date)
+
+    # Выход функции (сортировка по убыванию, т. е. сначала самые последние операции)
+    [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+    """
+    list_for_date = sorted(list_dictionaries, key=lambda x: x['date'], reverse=reverse)
+    return list_for_date
+
+
+#print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
