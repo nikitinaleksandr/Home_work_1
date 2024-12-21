43c14477ef14ea565dd8ea84485634ce5432ebb4
Aleksandr Nikitin <NAI_surgut@mail.ru>
2024-12-21 21:03:40 +0500

    test_read_trans_excel

---
+++ src/cvs_xlsx.py
@@ -0,0 +1,79 @@
+from csv import excel
+from idlelib.iomenu import encoding
+from pathlib import Path
+import csv
+import pandas as pd
+
+from src.utils import list_dict_transactions
+
+current_dir = Path(__file__).parent.parent.resolve()
+dir_transactions = current_dir/'transactions.csv'
+dir_transactions_excel = current_dir/'transactions_excel.xlsx'
+
+# print(dir_transactions)
+
+# df_csv = pd.read_csv("D:/transactions.csv")
+
+
+def read_csv_transactions(dir_transactions: str) -> list:
+    """
+    Функция для считывания финансовых транзакций из CSV,
+    принимает путь к файлу в качестве аргумента
+    результат выводится в виде списка словарей.
+    """
+    list_dict = []
+    with (open(dir_transactions, "r", encoding = "utf-8") as file):
+        reader = csv.DictReader(file)
+        for row in reader:
+            list_dict.append(row)
+            return list_dict
+            print(list_dict)
+
+read_csv_transactions(dir_transactions)
+
+
+def read_transactions_excel(dir_transactions: str) -> list:
+    """
+    Функция для считывания финансовых транзакций из Excel,
+    принимает путь к файлу Excel в качестве аргумента.
+    Результат выводится в виде списка словарей.
+    """
+    excel_data = pd.read_excel(dir_transactions_excel)
+    # print(excel_data)
+    excel_data_dict = excel_data.to_dict(orient='records')
+    return excel_data_dict
+    # print(excel_data_dict)
+
+
+read_transactions_excel(dir_transactions_excel)
+
+
+
+
+
+
+
+# df_csv = pd.read_csv("D:/transactions.csv")
+# print(df_csv.shape)
+# print(read_csv_transactions('transactions.csv'))
+#     df_csv = pd.read_csv("D:/transactions.csv")
+#
+#     return df_csv
+#
+# if __name__ == '__main__':
+#     print transactions = read_csv('transactions.csv')
+# df_csv = pd.read_csv("D:/transactions.csv")
+# print(df_csv)
+
+
+# def read_csv_transactions(transactions: str) -> list:
+#     df = pd.read_csv('dir_transactions.csv')
+
+
+# if __name__ == '__main__':
+#     read_csv_transactions(df_csv)
+
+# df = pd.read_excel("D:/transactions_excel.xlsx")
+# print(df.shape)
+# print(df.head())
+
---
+++ tests/test_read_transaction_excel.py
@@ -0,0 +1,8 @@
+from unittest.mock import patch
+import pandas as pd
+from src.cvs_xlsx import read_transactions_excel
+
+@patch('src.cvs_xlsx.pd.read_excel')
+def test_read_transactions_excel(mock_transactions_excel):
+    mock_transactions_excel.return_value = pd.DataFrame([{'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041'}])
+    assert read_transactions_excel('dir_transactions') == [{'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041'}]
